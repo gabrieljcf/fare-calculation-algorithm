32aeb71feaa0ef5ae503affc0700988ded3b0f1d
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-07-07 01:37:51 -0300

    refactor using factory

---
+++ src/FareCalculatorFactory.ts
@@ -0,0 +1,16 @@
+import NormalFareCalculator from "./NormalFareCalculator";
+import OverNightFareCalculator from "./OverNightFareCalculator";
+import OverNightSundayFareCalculator from "./OverNightSundayFareCalculator";
+import Segment from "./Segment";
+import SundayFareCalculator from "./SundayFareCalculator";
+
+export default class FareCalculatorFactory {
+  static create(segment: Segment) {
+    if (segment.isOverNight() && !segment.isSunday())
+      return new OverNightFareCalculator();
+    if (segment.isOverNight() && segment.isSunday())
+      return new OverNightSundayFareCalculator();
+    if (segment.isSunday()) return new SundayFareCalculator();
+    return new NormalFareCalculator();
+  }
+}
